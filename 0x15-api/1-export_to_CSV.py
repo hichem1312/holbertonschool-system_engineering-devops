154f02e1b2623cabac30df0f34264c40d05c4c32
hichem <hichemmaatoug55@gmail.com>
2022-09-12 11:40:15 -0700

    s

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,7 +9,6 @@ import csv
 
 if __name__ == "__main__":
     '''module'''
-    list = []
     t = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
                          .format(argv[1]))
     t = t.json()
@@ -19,8 +18,8 @@ if __name__ == "__main__":
     u = us["username"]
     f = open('./{}.csv'.format(argv[1]), 'w')
     wr = csv.writer(f, quoting=csv.QUOTE_ALL)
-    for i in range(0, len(t)):
-        if (((t[i]["userId"]) == int(argv[1]))):
+    for i in t:
+        if (((i["userId"]) == int(argv[1]))):
             wr.writerow([str(
-                t[i]["userId"]), u, t[i]["completed"], t[i]["title"]])
+                i["userId"]), u, i["completed"], i["title"]])
     f.close()
