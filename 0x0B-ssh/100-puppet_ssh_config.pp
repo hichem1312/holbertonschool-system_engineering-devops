5d1095d8efbee9652464706472f1336bcc89f50c
hichem <hichem@hichem.com>
2022-09-17 14:47:55 +0100

    x

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
-# connect to server without a password
+# connect to server without password
 file_line { 'ssh_config':
-    ensure   => present
-    path     => '/etc/ssh/ssh_config'
-    line     => 'PasswordAuthentication no'
+    ensure   => present,
+    path     => '/etc/ssh/ssh_config',
+    line     => 'PasswordAuthentication no',
     multiple => 'true'
 }
 file_line { 'ssh_config2':
-    ensure   => present
-    path     => '/etc/ssh/ssh_config'
-    line     => 'IdentityFile ~/.ssh/school'
+    ensure   => present,
+    path     => '/etc/ssh/ssh_config',
+    line     => 'IdentityFile ~/.ssh/school',
     multiple => 'true'
 }
