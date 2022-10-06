3643270cbcc68fdc94c85a145c6c1ad2c803fb21
hichem <hichem@hichem.com>
2022-10-06 18:03:14 +0100

    change OS configuration

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # Changing the OS configuration
 exec { 'hard':
-  command => 'sed "/holberton hard/s/5/20000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton hard/s/5/20000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'soft':
-  command => 'sed "/holberton soft/s/4/20000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton soft/s/4/20000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
